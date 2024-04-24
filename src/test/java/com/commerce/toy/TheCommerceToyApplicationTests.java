059ad97d6d957af1d55b2356378ba39053d95007
MinJung Kim <kminss.dev@gmail.com>
2024-04-24 16:44:06 +0900

    initial commit

---
+++ src/test/java/com/commerce/toy/TheCommerceToyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commerce.toy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheCommerceToyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
