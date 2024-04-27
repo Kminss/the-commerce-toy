cd346b37f7427cb42791e219e5337252a6e70d7a
MinJung Kim <kminss.dev@gmail.com>
2024-04-28 02:58:30 +0900

    fix: #10 - 회원가입 요청 Dto 유효성 검증 수정
    - id 유효성 검증 추가
    - not blank 추가

--- src/main/java/com/commerce/toy/domain/user/dto/request/JoinRequest.java
+++ src/main/java/com/commerce/toy/domain/user/dto/request/JoinRequest.java
@@ -1,7 +1,6 @@
 package com.commerce.toy.domain.user.dto.request;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
@@ -20,6 +19,7 @@ public class JoinRequest {
 		max = 30,
 		message = "아이디는 8자리 이상, 30자리 이하로 입력해야합니다."
 	)
+	@NotBlank(message = "아이디 입력은 필수 입니다.")
 	@Pattern(
 		regexp = "^[a-z0-9]+$",
 		message = "아이디는 알파벳 소문자, 숫자의 조합으로 입력해야합니다."
@@ -35,6 +35,7 @@ public class JoinRequest {
 		max = 20,
 		message = "비밀번호는 8자리 이상, 20자리 이하로 입력해야합니다."
 	)
+	@NotBlank(message = "비밀번호 입력은 필수 입니다.")
 	private String password; //비밀번호
 
 	@Size(
@@ -42,21 +43,22 @@ public class JoinRequest {
 		max = 20,
 		message = "닉네임은 3자리 이상, 20자리 이하로 입력해야합니다."
 	)
+	@NotBlank(message = "닉네임 입력은 필수 입니다.")
 	private String nickname; //닉네임
 
-	@NotBlank(message = "이름 입력은 필수 입니다.")
 	@Size(max = 30, message = "이름은 30자 이하로 입력해야합니다.")
+	@NotBlank(message = "이름 입력은 필수 입니다.")
 	private String name; //이름
 
-	@NotEmpty(message = "휴대전화번호 입력은 필수 입니다.")
 	@Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "유효하지 않은 휴대전화번호입니다.")
+	@NotBlank(message = "휴대전화번호 입력은 필수 입니다.")
 	private String phoneNumber; //휴대전화번호
 
-	@NotEmpty(message = "이메일 입력은 필수 입니다.")
 	@Pattern(
 		regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$",
 		message = "이메일 형식에 맞지 않습니다."
 	)
+	@NotBlank(message = "이메일 입력은 필수 입니다.")
 	private String email; //이메일
 
 	@Builder
