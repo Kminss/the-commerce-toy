0817babf21fc1841a3d84e8c3284671ff7f5026a
MinJung Kim <kminss.dev@gmail.com>
2024-04-28 02:20:21 +0900

    feat: #4 - 회원가입 API 구현

---
+++ src/main/java/com/commerce/toy/domain/user/service/UserService.java
@@ -0,0 +1,26 @@
+package com.commerce.toy.domain.user.service;
+
+import static com.commerce.toy.global.exception.ErrorCode.*;
+
+import org.springframework.stereotype.Service;
+
+import com.commerce.toy.domain.user.dto.request.JoinRequest;
+import com.commerce.toy.domain.user.service.repository.UserRepository;
+import com.commerce.toy.global.exception.ApiException;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+	private final UserRepository userRepository;
+	public void join(JoinRequest request) {
+		String email = request.getEmail();
+
+		if (userRepository.existsUserByEmail(email)) {
+			throw new ApiException(ALREADY_EXIST_USER);
+		}
+
+		userRepository.save(request.toEntity());
+	}
+}
