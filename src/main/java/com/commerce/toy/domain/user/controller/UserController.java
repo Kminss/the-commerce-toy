b36fb33d3e663db14f8df3dcbdc24b6c6e5a90cb
MinJung Kim <kminss.dev@gmail.com>
2024-04-28 17:23:24 +0900

    feat: #15 회원 목록 조회 API 구현

--- src/main/java/com/commerce/toy/domain/user/controller/UserController.java
+++ src/main/java/com/commerce/toy/domain/user/controller/UserController.java
@@ -4,8 +4,12 @@ import static org.springframework.http.HttpStatus.*;
 
 import javax.validation.Valid;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -16,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.commerce.toy.domain.user.dto.request.JoinRequest;
 import com.commerce.toy.domain.user.dto.request.UpdateUserRequest;
 import com.commerce.toy.domain.user.dto.response.UpdateUserResponse;
+import com.commerce.toy.domain.user.dto.response.UserInfoResponse;
 import com.commerce.toy.domain.user.service.UserService;
 import com.commerce.toy.global.response.ApiResponse;
 
@@ -41,5 +46,14 @@ public class UserController {
 	) {
 		UpdateUserResponse response = userService.update(id, request);
 		return ResponseEntity.ok(ApiResponse.of("회원 수정 성공", response));
+	}
+
+	@GetMapping("/list")
+	public ResponseEntity<ApiResponse<Page<UserInfoResponse>>> findAll(
+		@PageableDefault(size = 25, sort = "createdAt", direction = Sort.Direction.DESC)
+		Pageable pageable
+	) {
+		Page<UserInfoResponse> response = userService.findAll(pageable);
+		return ResponseEntity.ok(ApiResponse.of("회원 목록 조회 성공", response));
 	}
 }
--- src/main/java/com/commerce/toy/domain/user/service/repository/UserRepository.java
+++ src/main/java/com/commerce/toy/domain/user/service/repository/UserRepository.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.commerce.toy.domain.user.User;
+import com.commerce.toy.domain.user.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 	boolean existsUserById(String id);
